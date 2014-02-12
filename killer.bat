ca6ddce1488bff1b239768464eb2f5347f52f37e
xiaoyuanan2000 <xiaoyuanan2000@sina.com>
2014-02-12 12:28:45 +0800

    v2.2
    use batch!

---
+++ killer.bat
@@ -0,0 +1,24 @@
+@echo off
+set v=1.1
+cd /d "%~dp0"
+title NetChk killer v%v%
+if "%1"=="tray" goto tray
+mode con cols=45 lines=5
+echo  Killer v%v% - Support NetChk 2.0 to 2.2
+echo  -------------------------------------------
+echo  NetChk will close in a few seconds ...
+echo.>"%tmp%\netchk.close"
+sleep 50
+start /b "" "%~0" tray
+:t
+if exist "%tmp%\netchk.close" goto t
+echo  NetChk was closed successfully.
+exit
+:tray
+sleep 1750
+if exist "%tmp%\netchk.close" (
+echo  Still running? Press any key to kill it.
+pause>nul
+start /b cido /msg "Application killed." "NetChk Killer" 0
+taskkill /f /im cmd.exe>nul 2>nul)
+exit
---
+++ netchk.bat
@@ -0,0 +1,35 @@
+@echo off
+cd /d "%~dp0"
+if exist "%tmp%\netchk.close" del "%tmp%\netchk.close">nul
+set v=2.2
+ctray /del
+cido /sws 0
+ping /n 1 /4 114.114.114.114>nul
+set /a nowel=1-%errorlevel%
+start /b cido /msg "Application started." "NetChk v%v%" 0
+if "%nowel%"=="0" (ctray /add "NetChk %v%" /std 1
+) else ctray /add "NetChk %v%" /std 4
+:do
+if "%nowel%"=="1" (sleep 1500
+if exist "%tmp%\netchk.close" goto q
+sleep 1500
+if exist "%tmp%\netchk.close" goto q)
+sleep 1500
+if exist "%tmp%\netchk.close" goto q
+ping /n 1 /4 114.114.114.114>nul
+set /a el=1-%errorlevel%
+if not "%el%"=="%nowel%" (
+set nowel=%el%
+if "%el%"=="0" (ctray /edit "NetChk %v%" /std 1
+cido /beep 0
+start /b cido /msg "Network Disconnected." "NetChk v%v%" 0
+) else (ctray /edit "NetChk %v%" /std 4
+cido /beep 64
+start /b cido /msg "Network Established." "NetChk %v%" 0
+))
+goto do
+:q
+del %tmp%\netchk.close
+ctray /del
+start /b cido /msg "Application closed." "NetChk %v%" 0
+exit
