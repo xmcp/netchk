9a530e5adf8962d2fa705e82051b5dc238674f3d
xiaoyuanan2000 <xiaoyuanan2000@sina.com>
2014-02-12 12:33:08 +0800

    v2.3
    added time display

--- netchk.bat
+++ netchk.bat
@@ -1,12 +1,12 @@
 @echo off
 cd /d "%~dp0"
 if exist "%tmp%\netchk.close" del "%tmp%\netchk.close">nul
-set v=2.2
+set v=2.3
 ctray /del
 cido /sws 0
 ping /n 1 /4 114.114.114.114>nul
 set /a nowel=1-%errorlevel%
-start /b cido /msg "Application started." "NetChk v%v%" 0
+start /b cido /msg "Application started at %time%." "NetChk v%v%" 0
 if "%nowel%"=="0" (ctray /add "NetChk %v%" /std 1
 ) else ctray /add "NetChk %v%" /std 4
 :do
@@ -22,10 +22,10 @@ if not "%el%"=="%nowel%" (
 set nowel=%el%
 if "%el%"=="0" (ctray /edit "NetChk %v%" /std 1
 cido /beep 0
-start /b cido /msg "Network Disconnected." "NetChk v%v%" 0
+start /b cido /msg "Network Disconnected at %time%." "NetChk v%v%" 0
 ) else (ctray /edit "NetChk %v%" /std 4
 cido /beep 64
-start /b cido /msg "Network Established." "NetChk %v%" 0
+start /b cido /msg "Network Established at %time%." "NetChk %v%" 0
 ))
 goto do
 :q
